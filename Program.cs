8d7aad4cebae0c1e91b6894bb9d5f2d73bf639bb
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-23 00:34:38 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("digite o primeiro 1valor:");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o 2valor:");
+            valor2 = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine($"seu resultado é: {valor1 + valor2}");
+
+            Console.WriteLine("pressione enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
